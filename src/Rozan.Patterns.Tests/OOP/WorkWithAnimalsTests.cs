bfcd1a219c105bc53c01d934cabba67146de5ccc
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-09-25 23:46:33 -0300

    Tests: added animal inheritance

---
+++ src/Rozan.Patterns.Tests/OOP/WorkWithAnimalsTests.cs
@@ -0,0 +1,29 @@
+﻿using Rozan.Patterns.OOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozan.Patterns.Tests.OOP
+{
+    public class WorkWithAnimalsTests
+    {
+        [Test]
+        public void PlayWithCatAndDog()
+        {
+            // Arrange
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Act
+            dog.Name = "fido";
+            Console.WriteLine($"Dog's name is: {dog.Name}");
+
+            dog.DigHole();
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
