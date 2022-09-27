51c13f358ccfb2acf3ee32b19d8cb8480b3ab819
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-09-27 20:25:08 -0300

    created interfaces

---
+++ src/Rozan.Patterns.Behavioral/Observer/Interfaces/IObserver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozan.Patterns.Behavioral.Observer.Interfaces
+{
+    public interface IObserver
+    {
+        public void Update(
+            double ibmPrice, double aaplPrice, double googPrice
+            );
+    }
+}
