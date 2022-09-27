558d090225aac6780c55de025b4e5f967f45110b
Jairo Rozan <94655398+ZRozan@users.noreply.github.com>
2022-09-27 20:28:11 -0300

    Feature/observer (#3)
    * fixed namespace from earlier project
    
    * created interfaces
    
    * created tests
    
    * created grabber and observer

---
+++ src/Rozan.Patterns.Behavioral/Observer/Interfaces/ISubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozan.Patterns.Behavioral.Observer.Interfaces
+{
+    public interface ISubject
+    {
+        public void Register(IObserver o);
+        public void Unregister(IObserver o);
+        public void NotifyObserver();
+    }
+}
---
+++ src/Rozan.Patterns.Behavioral/Observer/StockObserver.cs
@@ -0,0 +1,48 @@
+﻿using Rozan.Patterns.Behavioral.Observer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozan.Patterns.Behavioral.Observer
+{
+    public class StockObserver : IObserver
+    {
+        private double _ibmPrice;
+        private double _aaplPrice;
+        private double _googPrice;
+
+        private static int _oberverIdTracker = 0;
+
+        private int _observerId;
+
+        private ISubject _stockGrabber;
+
+        public StockObserver(ISubject stockgrabber)
+        {
+            this._stockGrabber = stockgrabber;
+
+            this._observerId = ++_oberverIdTracker;
+
+            stockgrabber.Register(this);
+        }
+
+        public void Update(double ibmPrice, double aaplPrice, double googPrice)
+        {
+            this._ibmPrice = ibmPrice;
+            this._aaplPrice = aaplPrice;
+            this._googPrice = googPrice;
+
+            PrintPrices();
+        }
+
+        private void PrintPrices()
+        {
+            Console.WriteLine($"{_observerId}");
+            Console.WriteLine($"IBM: {_ibmPrice}");
+            Console.WriteLine($"Apple:{_aaplPrice}");
+            Console.WriteLine($"Google:{_googPrice}");
+        }
+    }
+}
