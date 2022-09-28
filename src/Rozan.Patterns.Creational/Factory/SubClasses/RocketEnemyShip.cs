f97ff383e2e463575dff0b1719f01bebe0747bf9
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-09-27 21:25:59 -0300

    created subclasses for factory

---
+++ src/Rozan.Patterns.Creational/Factory/SubClasses/RocketEnemyShip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozan.Patterns.Creational.Factory.SubClasses
+{
+    public class RocketEnemyShip : EnemyShip
+    {
+        public RocketEnemyShip()
+        {
+            Name = "Rocket Enemy Ship";
+            AmountDamage = 10.0;
+        }
+    }
+
+}
