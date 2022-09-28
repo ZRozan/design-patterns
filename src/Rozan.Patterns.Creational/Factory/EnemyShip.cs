085a88704b9e7e018840736f2fa76ffb60cedf04
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-09-27 21:27:15 -0300

    created enemy ship and factory

---
+++ src/Rozan.Patterns.Creational/Factory/EnemyShip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozan.Patterns.Creational.Factory
+{
+    public abstract class EnemyShip
+    {
+        private string _name;
+        private double _amountDamage;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public double AmountDamage
+        {
+            get { return _amountDamage; }
+            set { _amountDamage = value; }
+        }
+
+        public void FollowHeroShip()
+        {
+            Console.WriteLine($"{Name} is following the hero.");
+        }
+        public void DisplayEnemyShip()
+        {
+            Console.WriteLine($"{Name} is on screen.");
+        }
+        public void EnemyShipShoots()
+        {
+            Console.WriteLine($"{Name} attacks and does {AmountDamage}!");
+        }
+    }
+}
---
+++ src/Rozan.Patterns.Creational/Factory/EnemyShipFactory.cs
@@ -0,0 +1,26 @@
+﻿using Rozan.Patterns.Creational.Factory.SubClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozan.Patterns.Creational.Factory
+{
+    public class EnemyShipFactory
+    {
+        public EnemyShip MakeEnemyShip(string newShipType)
+        {
+            EnemyShip newShip = null;
+
+            if (newShipType == "1")
+                return new UfoEnemyShip();
+            else if (newShipType == "2")
+                return new RocketEnemyShip();
+            else if (newShipType == "3")
+                return new BigUfoEnemyShip();
+            else
+                return null;
+        }
+    }
+}
