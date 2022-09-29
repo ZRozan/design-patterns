a011c866188c6cf0928e0d2f6e8a8dbf0a19a2d3
Jairo Rozan <jairo.rosa.z@hotmail.com>
2022-09-29 20:51:50 -0300

    created Singleton pattern

---
+++ src/Rozan.Patterns.Tests/Creational/Singleton/SingletonTests.cs
@@ -0,0 +1,25 @@
+﻿using Rozan.Patterns.Creational.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozan.Patterns.Tests.Creational.Singleton
+{
+    public class SingletonTests
+    {
+        [Test]
+        public void GetTiles_RequestThreeTiles_ReturnsRandomizedThreeTiles()
+        {
+            int countExpected = 3;
+            var instance = ScrabbleSingleton.Instance;
+
+            var result = instance.GetLetterTiles(countExpected);
+            Console.WriteLine(String.Join(", ", result));
+
+            Assert.That(result.Count, Is.EqualTo(countExpected));
+            Assert.That(instance.AllLetersInBag.Count - countExpected, Is.EqualTo(instance.Availableletters.Count));
+        }
+    }
+}
